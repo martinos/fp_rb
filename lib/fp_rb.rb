f16e8d4fdae0fb25fa5a1f4459954a990924f702
Martin Chabot <chabotm@gmail.com>
2017-06-16 10:45:30 -0400

    adding utilities

--- lib/fp_rb.rb
+++ lib/fp_rb.rb
@@ -3,4 +3,11 @@ require "fp_rb/version"
 require 'fp_rb/operator'
 require 'fp_rb/maybe'
 require 'fp_rb/result'
+require 'active_support/core_ext/module/attribute_accessors'
 
+module FpRb
+  mattr_reader :and_then
+
+  @@and_then = -> f, a { a.and_then(f) }.curry
+  @@map = -> f, a { a.map(f) }.curry
+end
