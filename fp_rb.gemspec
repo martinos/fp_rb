ebaadf625565e432645fde3d64868c863f67db3a
Martin Chabot <chabotm@gmail.com>
2017-08-09 11:36:24 -0400

    removing ActiveSupport dependency

--- fp_rb.gemspec
+++ fp_rb.gemspec
@@ -22,7 +22,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency "superators19", "~>0.9.3"
-  spec.add_runtime_dependency "activesupport"
   spec.add_development_dependency "bundler", "~> 1.14"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
--- lib/fp_rb.rb
+++ lib/fp_rb.rb
@@ -3,11 +3,36 @@ require "fp_rb/version"
 require 'fp_rb/operator'
 require 'fp_rb/maybe'
 require 'fp_rb/result'
-require 'active_support/core_ext/module/attribute_accessors'
+
+class Module
+  # stolen from Rails mattr_accessor
+  def def_fn(*syms, instance_reader: true, instance_accessor: true, default: nil)
+    syms.each do |sym|
+      raise NameError.new("invalid attribute name: #{sym}") unless (sym =~ /\A[_A-Za-z]\w*\z/)
+      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+        @@#{sym} = nil unless defined? @@#{sym}
+        def self.#{sym}
+          @@#{sym}
+        end
+      EOS
+
+      if instance_reader && instance_accessor
+        class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+          def #{sym}
+            @@#{sym}
+          end
+        EOS
+      end
+
+      sym_default_value = (block_given? && default.nil?) ? yield : default
+      class_variable_set("@@#{sym}", sym_default_value) unless sym_default_value.nil?
+    end
+  end
+end
 
 module FpRb
-  mattr_reader :and_then
+  def_fn :and_then
 
-  @@and_then = -> f, a { a.and_then(f) }.curry
-  @@map = -> f, a { a.map(f) }.curry
+  @@and_then = ->f, a { a.and_then(f) }.curry
+  @@map = ->f, a { a.map(f) }.curry
 end
